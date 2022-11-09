7dbd6cfa88c4d86a0ce698e7fa3a3a4647ab4372
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2022-11-09 19:19:54 +0300

    Added multitran retrials

--- gd-mt-server.py
+++ gd-mt-server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, render_template
 import requests as rq
 from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError
+import sys
+
+
+MAX_ATTEMPTS = 16
 
 
 app = Flask(__name__, template_folder="./templates",
@@ -13,8 +18,23 @@ def serve_modified():
     lng_to = request.args.get('l2', default=2, type=int)
     text = request.args.get('s', default="hello", type=str)
 
-    r = rq.get(
-        f"https://www.multitran.com/m.exe?s={text}&l1={lng_from}&l2={lng_to}")
+    r = None
+    attempts = 0
+    while True:
+        try:
+            r = rq.get(
+                f"https://www.multitran.com/m.exe?s={text}&l1={lng_from}&l2={lng_to}")
+        except ConnectionError:
+            if attempts < MAX_ATTEMPTS:
+                attempts += 1
+                print(f"ConnectionError, retrials (with this one): {attempts}")
+                continue
+            else:
+                print("Too many connection attempts, shutting down the server...")
+                sys.exit(1)
+        else:
+            break
+
     multitran_html = r.text
 
     soup = BeautifulSoup(multitran_html, "html.parser")
